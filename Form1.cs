a95217a0295fd2a295213584b5e45a85837e3fa8
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-06-12 21:30:41 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TreeView_with_FIles
+{
+    public partial class Form1 : Form
+    {
+       static string rootPath = @"D:\Visual Studio ШАГ\C#\TreeView with FIles";
+       TreeNode rootNode = new TreeNode(Path.GetFileName(rootPath));
+
+        public Form1()
+        {
+            InitializeComponent();
+            BuildDirectoryTree(@"D:\Visual Studio ШАГ\C#\TreeView with FIles",rootNode);
+        }
+        private void BuildDirectoryTree(string path, TreeNode parentNode)
+        {
+            string[] directories = Directory.GetDirectories(path);
+
+            foreach (string directory in directories)
+            {
+                TreeNode node = new TreeNode(Path.GetFileName(directory));
+                parentNode.Nodes.Add(node);
+
+                BuildDirectoryTree(directory, node);
+            }
+            treeView1.Nodes.Add(rootNode);
+            BuildDirectoryTree(rootPath, rootNode);
+        }
+
+    }
+ }
